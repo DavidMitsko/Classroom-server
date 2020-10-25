0dbd5ab5106e6cadd742f315198e8575f5f1e8a4
DavidMitsko <david.mitko@mail.ru>
2020-10-25 14:19:32 +0300

    Changed time of mailing

--- src/main/java/by/mitsko/classroom/scheduler/ScheduledReport.java
+++ src/main/java/by/mitsko/classroom/scheduler/ScheduledReport.java
@@ -39,7 +39,7 @@ public class ScheduledReport {
         this.mailSender = mailSender;
     }
 
-    @Scheduled(cron = "0 03 14 ? * *")
+    @Scheduled(cron = "0 0 15 ? * *")
     public void prepareReports() {
         List<Report> reports = reportRepository.findAll();
 
